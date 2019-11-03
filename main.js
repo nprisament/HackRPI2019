e0112bad526da522c4edde08688a448c763b8466
hackerrank <me@hackerrank.com>
2019-11-02 22:50:56 -0400

    main switch

--- main.js
+++ main.js
@@ -5,12 +5,61 @@ $('#loading').show();
 
 parseColor = (danger) => {return 'red'};
 
-updatePage = (weights) => {
-	for (let i = 0; i < weights.length; i++) {
-		eventType = '';
-		switch(weights[i][1]) {
-			
+updatePage = (dangers) => {
+	for (let i = 0; i < dangers.length; i++) {
+		let eventType = '';
+		let eventDesc = '';
+		switch(dangers[i][1]) {
+			case 1:
+				eventType = 'Geophysical';
+				eventDesc = 'Geophysical (including landslide)';
+				break;
+			case 2:
+				eventType = 'Meteorological';
+				eventDesc = 'Meteorological (including flood)';
+				break;
+			case 3:
+				eventType = 'Safety';
+				eventDesc = 'General emergency and public safety';
+				break;
+			case 4:
+				eventType = 'Security';
+				eventDesc = 'Law enforcement, military, homeland and local/private security';
+				break;
+			case 5:
+				eventType = 'Rescue';
+				eventDesc = 'Rescue and recovery';
+				break;
+			case 6:
+				eventType = 'Fire';
+				eventDesc = 'Fire suppression and rescue';
+				break;
+			case 7:
+				eventType = 'Health';
+				eventDesc = 'Medical and public health';
+				break;
+			case 8:
+				eventType = 'Environemental';
+				eventDesc = 'Pollution and other environmental';
+				break;
+			case 9:
+				eventType = 'Transport';
+				eventDesc = 'Public and private transportation';
+				break;
+			case 10:
+				eventType = 'Infrastructure';
+				eventDesc = 'Utility, telecommunication, other non-transport infrastructure';
+				break;
+			case 11:
+				eventType = 'CBRNE'
+				eventDesc = 'Chemical, Biological, Radiological, Nuclear or High-Yield Explosive threat';
+				break;
+			case 12:
+				eventType = 'Other';
+				break;
 		}
+		$('#Event Type-' + eventType);
+		$('#Event Description-' + eventDesc);
 		$('#dangerType-' + i).text(eventType);		
 		$('#danger-' + i).text(weights[i][0]).css('color', parseColor(weights[i][0]));
 	}
@@ -22,4 +71,4 @@ locationError = () => {
 
 };
 
-getDanger().then(updatePage).catch(locationError);
+getAlerts(getDanger, updatePage);
