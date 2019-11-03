756704971d48abecc0a8924889a283fd203c3ad8
hackerrank <me@hackerrank.com>
2019-11-03 11:51:40 -0500

    ibc cloud attempt 1

---
+++ vcap-local.js
@@ -0,0 +1,12 @@
+{
+    "services": {
+      "cloudantNoSQLDB": [
+        {
+          "credentials": {
+            "url":"crn:v1:bluemix:public:cloudantnosqldb:us-east:a/0ace91819b8947f5a03d35aa08823a8a:26fa6fb2-a443-4ace-b059-f5c787c16dee::"
+          },
+          "label": "cloudantNoSQLDB"
+        }
+      ]
+    }
+  }
