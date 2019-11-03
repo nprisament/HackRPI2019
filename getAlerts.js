1638cdea66e6e917c49254f90105f654ab3ffc46
Noah Theodore Prisament <nprisament@gmail.com>
2019-11-02 22:02:00 -0400

    for more dangers

--- getAlerts.js
+++ getAlerts.js
@@ -1,8 +1,6 @@
-
 //https://api.weather.com/v1/country/CA/alerts.json?language=en-US&apiKey=9d2908c81003444ea908c81003b44ed4
 // do something awesome that makes the world a better place
-
-module.export = function getInfo(){
+module.export = function getAlerts(){
     $.getJSON('http://gd.geobytes.com/GetCityDetails?callback=?&fqnc=', function(data) {
         console.log(data);
         fetch('https://cors-anywhere.herokuapp.com/'+`https://api.weather.com/v1/country/${data.geobytesinternet}/alerts.json?language=en-US&apiKey=9d2908c81003444ea908c81003b44ed4`, {
--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-getDanger = () => new Promise(function(resolve, reject) {
-	resolve({danger: 5}); 
-});
+import getDanger from './getDanger';
 
-
-
+$('#dangers').hide();
+$('#loading').show();
 
 parseColor = (danger) => {return 'red'};
 
-updatePage = (report) => {
-	$('#danger-level').text(report.danger).css('color', parseColor(report.danger));
+updatePage = (weights) => {
+	for (let i = 0; i < weights.length; i++) {
+		eventType = '';
+		switch(weights[i][1]) {
+			
+		}
+		$('#dangerType-' + i).text(eventType);		
+		$('#danger-' + i).text(weights[i][0]).css('color', parseColor(weights[i][0]));
+	}
+	$('#loading').hide();
+	$('#dangers').show();
 }
 
 locationError = () => {
